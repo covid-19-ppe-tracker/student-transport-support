f3eb3902dc94786eee7e0622ecd6ff3530e4ffb1
Shinjan Mitra <shinjanxp@gmail.com>
2020-04-11 11:57:16 +0530

    refactor: Convert routes in index.js to async-await

--- routes/index.js
+++ routes/index.js
@@ -31,64 +31,73 @@ router.get('/ppe/map', function (req, res, next) {
   res.render('ppe-map', { lat: req.query.lat || 22, lng: req.query.lng || 84, zoom: req.query.zoom || 4.5 });
 });
 // View ppe as list
-router.get('/ppe/list', function (req, res, next) {
-  models.Availability.findAll({ include: models.PPEType }).then(function (availabilities) {
-    models.Requirement.findAll({ include: models.PPEType }).then(function (requirements) {
-      models.Manufacturing.findAll({ include: models.PPEType }).then(function (manufacturing) {
-        res.render('ppe-list', { availabilities: availabilities, requirements: requirements, manufacturing: manufacturing });
-      }).catch(function (err) {
-        console.log('Oops! something went wrong, : ', err);
-      });
-    }).catch(function (err) {
-      console.log('Oops! something went wrong, : ', err);
-    });
-  }).catch(function (err) {
-    console.log('Oops! something went wrong, : ', err);
-  });
+router.get('/ppe/list', async function (req, res, next) {
+  try {
+    const availabilities = await models.Availability.findAll({ include: models.PPEType });
+    const requirements = await models.Requirement.findAll({ include: models.PPEType });
+    const manufacturings = await models.Manufacturing.findAll({ include: models.PPEType });
+    res.render('ppe-list', { availabilities: availabilities, requirements: requirements, manufacturing: manufacturings });
+  }
+  catch (e) {
+    next(e)
+  }
 });
 
 // View ppe-create form
-router.get('/ppe/create', function (req, res, next) {
-  models.PPEType.findAll()
-    .then(function (PPETypes) {
-      res.render('ppe-create', { PPETypes: PPETypes });
-    })
+router.get('/ppe/create', async function (req, res, next) {
+  try {
+    const PPETypes = await models.PPEType.findAll();
+    res.render('ppe-create', { PPETypes: PPETypes });
+  } catch (e) {
+    next(e);
+  }
 });
 // View contact-us information
 router.get('/contact-us', function (req, res, next) {
   res.render('contact-us');
 });
-router.post('/suggestion', function (req, res, next) {
-  models.Suggestion.create({
-    text: req.body.suggestion
-  })
-    .then(function (created) {
-      res.render('contact-us', { alert: true });
-    })
+router.post('/suggestion', async function (req, res, next) {
+  try {
+    await models.Suggestion.create({
+      text: req.body.suggestion
+    });
+    res.render('contact-us', { alert: true });
+  } catch (e) {
+    next(e);
+  }
 })
 // Get list of availabilities
-router.get('/availability', function (req, res, next) {
-  models.Availability.findAll({ attributes: ['name', 'quantity', 'latitude', 'longitude'], include: models.PPEType }).then(function (items) {
-    res.send(items);
-  }).catch(function (err) {
-    console.log('Oops! something went wrong, : ', err);
-  });
+router.get('/availability', async function (req, res, next) {
+  try {
+    const availabilities = await models.Availability.findAll(
+      { attributes: ['name', 'quantity', 'latitude', 'longitude'], include: models.PPEType }
+    );
+    res.send(availabilities);
+  } catch (e) {
+    next(e);
+  }
 });
 // Get list of requirements
-router.get('/requirement', function (req, res, next) {
-  models.Requirement.findAll({ attributes: ['name', 'quantity', 'latitude', 'longitude'], include: models.PPEType }).then(function (items) {
-    res.send(items);
-  }).catch(function (err) {
-    console.log('Oops! something went wrong, : ', err);
-  });
+router.get('/requirement', async function (req, res, next) {
+  try {
+    const requirements = await models.Requirement.findAll(
+      { attributes: ['name', 'quantity', 'latitude', 'longitude'], include: models.PPEType }
+    );
+    res.send(requirements);
+  } catch (e) {
+    next(e);
+  }
 });
 // Get list of manufacturings
-router.get('/manufacturing', function (req, res, next) {
-  models.Manufacturing.findAll({ attributes: ['name', 'quantity', 'latitude', 'longitude'], include: models.PPEType }).then(function (items) {
-    res.send(items);
-  }).catch(function (err) {
-    console.log('Oops! something went wrong, : ', err);
-  });
+router.get('/manufacturing', async function (req, res, next) {
+  try {
+    const manufacturings = await models.Manufacturing.findAll(
+      { attributes: ['name', 'quantity', 'latitude', 'longitude'], include: models.PPEType }
+    );
+    res.send(manufacturings);
+  } catch (e) {
+    next(e);
+  }
 });
 function findMatches(newPost, newPostType, mode) {
   let searchType;
@@ -128,7 +137,7 @@ router.post('/ppe',
     check("uri", "hyperlink 'uri' must be present and conform to a URL").if((value, { req }) => { return req.body.kind == "hyperlink"; }).exists().isURL()
   ]),
   async function (req, res, next) {
-    
+
     try {
       const proof = await models.Proof.create();
       const document = await models.Document.create({
@@ -171,29 +180,28 @@ router.post('/ppe',
 //   res.render('ppe-thanks');
 // });
 
-router.post('/ppe/save-subscription/', function (req, res) {
-  models.Subscription.create({
-    forId: req.body.forId,
-    forType: req.body.forType,
-    pushSubscription: req.body.pushSubscription
-  })
-    .then(function (subscriptionId) {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({ data: { success: true } }));
-      models[req.body.forType].findOne({ where: { id: req.body.forId } }).then(function (res) {
-        findMatches(res, req.body.forType, 'onSubscribe');
-      })
-    })
-    .catch(function (err) {
-      res.status(500);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({
-        error: {
-          id: 'unable-to-save-subscription',
-          message: 'The subscription was received but we were unable to save it to our database.'
-        }
-      }));
+router.post('/ppe/save-subscription/', async function (req, res) {
+  try {
+    const sub = await models.Subscription.create({
+      forId: req.body.forId,
+      forType: req.body.forType,
+      pushSubscription: req.body.pushSubscription
     });
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ data: { success: true } }));
+    const application = await models[req.body.forType].findOne({ where: { id: req.body.forId } })
+    findMatches(application, req.body.forType, 'onSubscribe');
+
+  } catch (e) {
+    res.status(500);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+      error: {
+        id: 'unable-to-save-subscription',
+        message: 'The subscription was received but we were unable to save it to our database.'
+      }
+    }));
+  }
 });
 
 const triggerPushMsg = function (subscription, dataToSend) {
@@ -225,45 +233,7 @@ function sendMessage(recipientId, recipientType, coords) {
       return promiseChain;
     })
 }
-/*
-function shouldSend(subscription) {
-  return true;
-}
-router.get('/ppe/trigger-push', function (req, res, next) {
-  models.Subscription.findAll()
-    .then(function (subscriptions) {
-      let promiseChain = Promise.resolve();
 
-      for (let i = 0; i < subscriptions.length; i++) {
-        const subscription = subscriptions[i];
-        if (shouldSend(subscription)) {
-          promiseChain = promiseChain.then(() => {
-            let payload = { title: "COVID-19 PPE TRacker", message: "We found a match" };
-
-            return triggerPushMsg(JSON.parse(subscription.pushSubscription), JSON.stringify(payload));
-          });
-        }
-      }
-
-      return promiseChain;
-    })
-    .then(() => {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({ data: { success: true } }));
-    })
-    .catch(function (err) {
-      res.status(500);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({
-        error: {
-          id: 'unable-to-send-messages',
-          message: `We were unable to send messages to all subscriptions : ` +
-            `'${err.message}'`
-        }
-      }));
-    });
-})
-*/
 
 
 module.exports = router;
